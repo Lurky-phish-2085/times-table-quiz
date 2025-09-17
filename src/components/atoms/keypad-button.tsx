35ea2fc1c5a6f3ea48c8daaf7d3e976e32f067c9
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-09-17 15:44:19 +0800

    feat: add visual glow to KeypadButton on corresponding key press
    KeypadButton listens to window keydown and keyup events to glow itself
    to make it look like it was clicked visually when it's corresponding
    keyboard key is pressed. This enhances user feedback and keyboard
    accessibility.

--- src/components/atoms/keypad-button.tsx
+++ src/components/atoms/keypad-button.tsx
@@ -1,18 +1,43 @@
 import { clsx } from "clsx";
-import type { ButtonHTMLAttributes, RefAttributes } from "react";
+import { useState, type ButtonHTMLAttributes, type RefAttributes } from "react";
+import { useEventListener } from "usehooks-ts";
 
 type KeypadButtonProps = {
-  highlighted?: boolean;
+  glowOnKey?: string;
 }
   & ButtonHTMLAttributes<HTMLButtonElement>
   & RefAttributes<HTMLButtonElement>;
 
 function KeypadButton({
-  highlighted,
   className,
+  glowOnKey,
+  onClick,
   ref,
   ...props
 }: KeypadButtonProps) {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const setToInactive = () => setTimeout(() => setIsActive(false), 80);
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    const { key } = event;
+
+    if (key === glowOnKey) {
+      setIsActive(true);
+    }
+  };
+  const onKeyUp = (event: KeyboardEvent) => {
+    const { key } = event;
+
+    if (key === glowOnKey) {
+      setToInactive();
+    }
+  };
+
+  if (glowOnKey) {
+    useEventListener("keydown", onKeyDown);
+    useEventListener("keyup", onKeyUp);
+  }
 
   return (
     <button
@@ -22,10 +47,11 @@ function KeypadButton({
       className={clsx(
         "py-6 px-10 border border-black rounded-md",
         "transition-colors hover:bg-blue-800/20 active:bg-blue-800/45",
-        { "bg-blue-800/45": highlighted },
+        { "bg-blue-800/45": isActive },
         "text-center uppercase text-xl font-bold",
         className,
       )}
+      onMouseDown={(e) => { e.preventDefault(); onClick?.call(undefined, e) }}
     >
     </button>
   );

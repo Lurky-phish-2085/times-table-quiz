f75bb27029375b096900801a900f49f5965e5220
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-09-17 17:58:31 +0800

    fix: Fix useAnswerInput hook to only allow number values

--- src/components/organisms/times-table-quiz-app/hooks.ts
+++ src/components/organisms/times-table-quiz-app/hooks.ts
@@ -1,31 +1,32 @@
-import { useState, type Dispatch, type SetStateAction } from "react";
+import { useState } from "react";
 
 const MAX_INPUT_VALUE_LENGTH = 3;
 
 type UseAnswerInputHookType = {
   inputValue: string;
-  setInputValue: Dispatch<SetStateAction<string>>;
+  setInputValue: (value: string) => void;
   append: (value: string) => void;
   backspace: () => void;
   clear: () => void;
 };
 
 export const useAnswerInput = (): UseAnswerInputHookType => {
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setValue] = useState<string>("");
 
   const isNumber = (number: string) => Number.isFinite(Number(number));
   const isInputValueMaxLength = inputValue.length === MAX_INPUT_VALUE_LENGTH;
 
-  const append = (value: string) => {
+  const setInputValue = (value: string) => {
     if (isInputValueMaxLength) return;
 
     if (isNumber(value)) {
-      setInputValue((prev) => prev + value);
+      setValue(value);
     }
-  }
+  };
 
-  const backspace = () => setInputValue((prev) => prev.slice(0, -1));
-  const clear = () => setInputValue("");
+  const append = (value: string) => setInputValue(inputValue + value);
+  const backspace = () => setValue((prev) => prev.slice(0, -1));
+  const clear = () => setValue("");
 
   return {
     inputValue,

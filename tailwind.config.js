238020d0df80e8ff2afa7b07d10dea4d8b879406
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-09-17 17:47:12 +0800

    feat: Add custom global tailwindcss color classes
    All common colors found in the app will be defined here and follows
    material ui color naming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#FECACA", // red-200
+          DEFAULT: "#EF4444", // red-500
+          dark: "#B91C1C",    // red-700
+        },
+        secondary: {
+          light: "#FFFFFF",   // white
+          DEFAULT: "#FFFFFF", // white
+          dark: "#1F2937",    // gray-800
+        },
+        background: {
+          light: "#F3F4F6",   // gray-100
+          dark: "#111827",    // gray-900
+        },
+        surface: {
+          light: "#FFFFFF",   // white
+          dark: "#1E293B",    // slate-800
+        },
+        text: {
+          light: "#1F2937",   // gray-800
+          dark: "#E5E7EB",    // gray-200
+        },
+      },
+    },
   },
   plugins: [],
 }

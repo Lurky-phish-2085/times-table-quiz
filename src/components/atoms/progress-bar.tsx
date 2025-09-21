7dbd4e8ce95d036496d588d3fde877f06b57b665
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-09-21 15:19:45 +0800

    feat: Implement ProgressBar

---
+++ src/components/atoms/progress-bar.tsx
@@ -0,0 +1,49 @@
+import { clsx } from "clsx";
+import { useEffect, useRef } from "react";
+
+const MIN_PROGRESS_VALUE = 0;
+const MAX_PROGRESS_VALUE = 100;
+
+const clamp = (value: number, min: number, max: number) =>
+  Math.floor(Math.min(Math.max(value, min), max));
+
+type ProgressBarProps = {
+  progress: number;
+};
+
+function ProgressBar({
+  progress,
+}: ProgressBarProps) {
+  const innerBarRef = useRef<HTMLDivElement>(null);
+
+  const innerBarWidth = `${clamp(progress, MIN_PROGRESS_VALUE, MAX_PROGRESS_VALUE)}`;
+
+  useEffect(() => {
+    if (innerBarRef?.current) {
+      innerBarRef.current.style.width = `${innerBarWidth}%`;
+    }
+  }, [progress]);
+
+  return (
+    <div
+      className={clsx(
+        "h-[0.2rem]",
+        "border rounded-full",
+        "bg-neutral-100",
+      )}
+    >
+      <div
+        ref={innerBarRef}
+        className={clsx(
+          "h-full",
+          "bg-primary",
+          "transition-all, duration-500",
+        )}
+      >
+        {innerBarWidth}
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;

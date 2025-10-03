f6912ee77708130f809597a4cdd4de1a1909bbf5
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-10-03 23:06:11 +0800

    enhance: Make the paragraphs of ResultMedal bigger

--- src/components/molecules/result-medal.tsx
+++ src/components/molecules/result-medal.tsx
@@ -31,7 +31,7 @@ function ResultMedal({
         {title}
       </h1>
       <p
-        className="w-80 text-lg text-center"
+        className="w-80 text-xl text-center"
       >
         {subTitleMessage}
       </p>

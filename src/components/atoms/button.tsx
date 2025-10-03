1bec709f4a93760167889ac0d81e7da48e1464df
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-10-03 16:54:08 +0800

    feat: Add new icon neutral button style variant

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,8 +1,21 @@
 import clsx from "clsx";
 import type { ButtonHTMLAttributes, RefAttributes } from "react";
 
-const BUTTON_COMMON_STYLES: string = clsx(
+const NEUTRAL_COLOR_STYLES: string = clsx(
+  "border-neutral-400",
+  "text-neutral-400",
+  "hover:bg-neutral-400 hover:text-white",
+);
+
+const BUTTON_BOX_STYLE: string = clsx(
   "px-8 py-4",
+);
+
+const ICON_BUTTON_BOX_STYLE: string = clsx(
+  "py-4 px-4",
+);
+
+const BUTTON_COMMON_STYLES: string = clsx(
   "border-2 rounded-full",
   "font-extrabold",
   "transition-colors",
@@ -10,6 +23,7 @@ const BUTTON_COMMON_STYLES: string = clsx(
 );
 
 const FILLED_PRIMARY_BUTTON_STYLES: string = clsx(
+  BUTTON_BOX_STYLE,
   BUTTON_COMMON_STYLES,
   "border-primary",
   "text-white",
@@ -18,6 +32,7 @@ const FILLED_PRIMARY_BUTTON_STYLES: string = clsx(
 );
 
 const OUTLINE_PRIMARY_BUTTON_STYLES: string = clsx(
+  BUTTON_BOX_STYLE,
   BUTTON_COMMON_STYLES,
   "border-primary",
   "text-primary",
@@ -25,16 +40,23 @@ const OUTLINE_PRIMARY_BUTTON_STYLES: string = clsx(
 );
 
 const OUTLINE_NEUTRAL_BUTTON_STYLES: string = clsx(
+  BUTTON_BOX_STYLE,
   BUTTON_COMMON_STYLES,
-  "border-neutral-400",
-  "text-neutral-400",
-  "hover:bg-neutral-400 hover:text-white",
+  NEUTRAL_COLOR_STYLES,
+);
+
+const ICON_OUTLINE_NEUTRAL_BUTTON_STYLES: string = clsx(
+  ICON_BUTTON_BOX_STYLE,
+  BUTTON_COMMON_STYLES,
+  NEUTRAL_COLOR_STYLES,
 );
 
 type ButtonVariants =
   "filled-primary"
   | "outline-primary"
-  | "outline-neutral";
+  | "outline-neutral"
+  | "icon-outline-neutral";
+
 
 type ButtonProps = {
   variant: ButtonVariants;
@@ -48,10 +70,22 @@ function Button({
   ref,
   ...props
 }: ButtonProps) {
-  const styles =
-    variant === "filled-primary" ? FILLED_PRIMARY_BUTTON_STYLES :
-      variant === "outline-primary" ? OUTLINE_PRIMARY_BUTTON_STYLES :
-        variant === "outline-neutral" ? OUTLINE_NEUTRAL_BUTTON_STYLES : ""
+  let styles = "";
+
+  switch (variant) {
+    case "filled-primary":
+      styles = FILLED_PRIMARY_BUTTON_STYLES;
+      break;
+    case "outline-primary":
+      styles = OUTLINE_PRIMARY_BUTTON_STYLES;
+      break;
+    case "outline-neutral":
+      styles = OUTLINE_NEUTRAL_BUTTON_STYLES;
+      break;
+    case "icon-outline-neutral":
+      styles = ICON_OUTLINE_NEUTRAL_BUTTON_STYLES;
+      break;
+  }
 
   return (
     <button

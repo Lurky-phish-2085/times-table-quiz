149e382732fc94a6828b4f19bd4e5ada714e2aea
lurky-phish-2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-10-04 19:12:38 +0800

    feat: Define new text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,11 +24,23 @@ export default {
         },
         surface: {
           light: "#FFFFFF",   // white
+          DEFAULT: "#F5F5F5", // neutral-100
           dark: "#1E293B",    // slate-800
         },
+        neutral: {
+          light: "#F5F5F5", // neutral-100
+          DEFAULT: "#737373", // neutral-500
+          dark: "#404040", // neutral-700
+        },
         text: {
-          light: "#1F2937",   // gray-800
-          dark: "#E5E7EB",    // gray-200
+          light: "#FFFFFF",   // white
+          DEFAULT: "#FFFFFF", // white
+          dark: "#000000",    // black
+          green: {
+            light: "#86EFAC", // green-300
+            DEFAULT: "#16A34A", // green-600
+            dark: "#16A34A", // green-600
+          },
         },
       },
     },
